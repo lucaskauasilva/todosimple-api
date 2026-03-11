801329edbc284811a300abd50114692673c133b9
Lucas <lukaskaua172003@gmail.com>
2026-03-11 19:34:58 -0300

    Create UserService

--- src/main/java/com/lucaskaua/todosimple/repositories/UserRepository.java
+++ src/main/java/com/lucaskaua/todosimple/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.lucaskaua.todosimple.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.lucaskaua.todosimple.models.User;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/lucaskaua/todosimple/services/UserService.java
@@ -0,0 +1,53 @@
+package com.lucaskaua.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lucaskaua.todosimple.models.User;
+import com.lucaskaua.todosimple.repositories.TaskRepository;
+import com.lucaskaua.todosimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não encontrado com o id: " + id + ", Tipo: " + User.class.getName()));
+
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não é possível excluir o usuário, pois há entidades relacionadas!");
+        }
+    }
+
+}
