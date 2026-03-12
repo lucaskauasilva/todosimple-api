731f5a3226f9350b20288671a04ff2cdbcd9faf8
Lucas <lukaskaua172003@gmail.com>
2026-03-12 15:01:40 -0300

    Refactor UserService

--- src/main/java/com/lucaskaua/todosimple/services/UserService.java
+++ src/main/java/com/lucaskaua/todosimple/services/UserService.java
@@ -16,9 +16,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
@@ -30,7 +27,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
